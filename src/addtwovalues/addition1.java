dedef95ebec5dc3a86a969372cf588796663e371
Anumolthampi <anu.thampi29@gmail.com>
2023-07-09 18:46:47 -0400

    adding two values

---
+++ src/addtwovalues/addition1.java
@@ -0,0 +1,18 @@
+package addtwovalues;
+
+public class addition1 {
+
+		
+		int a;
+		int b;
+		
+	void addition()
+	{
+		
+		int sum= a+b;
+		
+		System.out.println("Addition of two numbers ="+ sum);
+		
+		}
+
+}
---
+++ src/addtwovalues/addition1Main.java
@@ -0,0 +1,12 @@
+package addtwovalues;
+
+
+public class addition1Main {
+	public static void main(String[] args) 
+	{
+		addition1 value = new addition1();
+		value.a= 15;
+		value.b= 12;
+		value.addition();
+	}
+}
