61332efa05ad8b91ece3b00397d638254137ab13
Anumolthampi <anu.thampi29@gmail.com>
2023-07-10 21:57:50 -0400

    Account login main

--- src/loginDetails/ConsoleLoginMain.java
+++ src/loginDetails/ConsoleLoginMain.java
@@ -8,24 +8,20 @@ public class ConsoleLoginMain {
 		// TODO Auto-generated method stub
 
 		ConsoleLogin chances = new ConsoleLogin();
-		
-				Scanner sc = new Scanner(System.in);
-				
-				chances.attempts=3;
-				chances.password= "Admin123";
-				chances.userId= "pivotAdmin";
-				
-				
-				
-					System.out.println("Enter your Username");
-					chances.userId = sc.nextLine();
-					
-			
-					System.out.println("Enter your password");
-					chances.password = sc.nextLine();
-					
-				
-				chances.checkUserId();
+
+		Scanner sc = new Scanner(System.in);
+
+		chances.attempts = 3;
+		chances.password = "Admin123";
+		chances.userId = "pivotAdmin";
+
+//		System.out.println("Enter your UserId");
+//		String enteredUserId = sc.nextLine();
+//
+//		System.out.println("Enter your password");
+//    	String enteredPassword = sc.nextLine();
+
+		chances.checkUserId(null, null);
 	}
 
 }
