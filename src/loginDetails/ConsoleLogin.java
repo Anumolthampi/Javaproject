da71a199aac6f74083099f1a0882f1f082b1ef0f
Anumolthampi <anu.thampi29@gmail.com>
2023-07-10 21:56:42 -0400

    Account LOgin

--- src/loginDetails/ConsoleLogin.java
+++ src/loginDetails/ConsoleLogin.java
@@ -1,31 +1,43 @@
 package loginDetails;
 
+import java.util.Scanner;
+
 public class ConsoleLogin {
 
 	String userId;
 	String password;
 	int attempts;
+	Scanner sc = new Scanner(System.in);
 
-	void checkUserId() {
+	void checkUserId(String enteredUserId, String enteredPassword) {
 
-		while (attempts > 0) {
-			if (userId.equals(userId) && (password.equals(password)))
+		while (attempts > 0) 
+		
+{
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("Enter your Username");
+			enteredUserId= sc.nextLine();
 
-			{
-				System.out.println("Login was succesfull");
+			System.out.println("Enter your password");
+	    	enteredPassword = sc.nextLine();
+
+			if (userId.equals(enteredUserId) && (password.equals(enteredPassword))) {
+				System.out.println("Login succesfull, You are now Logged in to the application");
 
 				break;
-			} else 
-			{
-				attempts--;
-				System.out.println("Invalid user name or password");
 
+			} else {
+				System.out.println("Invalid user name or password");
+				attempts--;
 				System.out.println("remaining attempts: " + attempts);
 			}
-		}
-		if (attempts == 0) {
-			System.out.println("Account locked");
+}
+			if (attempts == 0) {
+				System.out.println("Account locked");
+			}
 		}
 
 	}
-}
+
+
